7ae22d88ad43261df0f5b1bf9d743c759a3b6b23
Ilham Ibnu Pranata <ilhamibnupranata@students.amikom.ac.id>
2023-06-09 20:03:23 +0700

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg border">
       <div class="container d-flex">
         <a class="navbar-brand" href="#"> 
-          <img src="../assets/logo2.png" alt="" width="100" height="70" />
+          <img src="../assets/logo2.png" alt="" width="150" height="120" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -12,11 +12,17 @@
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Features</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Upload</a></li>
+                  <li><a class="dropdown-item" href="#">Download</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Setting</a></li>
+                </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="#">News</a>
             </li>
           </ul>
         </div>
